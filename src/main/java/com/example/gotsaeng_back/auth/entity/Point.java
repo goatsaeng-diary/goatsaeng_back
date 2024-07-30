5b0c43ea3b7fb5050a871c8853f4953cb4cc7a30
h_jinnny <nd3807@naver.com>
2024-07-30 20:44:34 +0900

    Feat: 챌린지, 포인트 엔티티 생성

--- src/main/java/com/example/gotsaeng_back/auth/entity/Point.java
+++ src/main/java/com/example/gotsaeng_back/auth/entity/Point.java
@@ -1,4 +1,30 @@
 package com.example.gotsaeng_back.auth.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name ="points")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Point {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "point_id")
+    private Long pointId;
+
+    @Column(name = "point_history")
+    private String pointHistory;
+
+    @Column(name = "get_date")
+    private LocalDateTime getDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
--- src/main/java/com/example/gotsaeng_back/challenge/entity/Challenge.java
+++ src/main/java/com/example/gotsaeng_back/challenge/entity/Challenge.java
@@ -1,4 +1,32 @@
 package com.example.gotsaeng_back.challenge.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "challenges")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Challenge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "challenge_id")
+    private Long challengeId;
+
+    private String name;
+
+    private String content;
+
+    private Long point;
+
+    @Column(name = "started_date")
+    private LocalDateTime startedDate;
+
+    @Column(name = "ended_date")
+    private LocalDateTime endedDate;
 }
