14b1e90dd54defd23258acd6102855024ee6397a
h_jinnny <nd3807@naver.com>
2024-08-24 12:41:24 +0900

    feat: 포인트 기능 추가

---
+++ src/main/java/com/example/gotsaeng_back/domain/study/dto/StudyDto.java
@@ -0,0 +1,12 @@
+package com.example.gotsaeng_back.domain.study.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class StudyDto {
+    private String answer;
+}
